f7691e509dbce3b84ff178812cb1cc664bf4f181
Bertrand RENAUDIN <bertrand.renaudin@odigo.com>
2022-12-14 14:58:10 +0100

    Version 0.1 Date : 14/12/2022

---
+++ app.js
@@ -0,0 +1,17 @@
+const fetchPokemons = () => {
+  const promises = [];
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+        console.log(pokemon);
+  });
+};
+fetchPokemons();
