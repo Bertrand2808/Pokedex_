727ae92d162c4a8bacdb4f154ba6f5c6760a1767
Bertrand RENAUDIN <bertrand.renaudin@odigo.com>
2022-12-16 16:39:34 +0100

    16/12

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
 const pokedex = document.getElementById('pokedex');
-console.log(pokedex);
 
-const fetchPokemons = () => {
-  const promises = [];
-  for (let i = 1; i <= 150; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
-    }
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
-        }));
-        console.log(results);
-        displayPokemon(pokemon);
-      });
-    };
+const fetchPokemon = async () => {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const pokemon = data.results.map((result, index) => ({
 
-    const displayPokemon = (pokemon) => {
-      console.log(pokemon);
-      const pokemonHTMLString = pokemon.map ( pokeman =>
+    ...result,
+    id: index + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`,
+  }));
+  displayPokemon(pokemon);
+};
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokeman) =>
         `
-      <li class="card">
+      <li class="card" onclick="selectPokemon(${pokeman.id})">
         <img class="card-image" src="${pokeman.image}"/>
         <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-        <p class="card-subtitle">Type : ${pokeman.type}</p>
       </li>
       `
-      ).join('');
-      pokedex.innerHTML = pokemonHTMLString;
-}
+    )
+    .join('');
+  pokedex.innerHTML = pokemonHTMLString;
+};
 
-fetchPokemons();
+const selectPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokeman = await res.json();
+  displayPopup(pokeman);
+};
+const displayPopup = (pokeman) => {
+  console.log(pokeman);
+  const type = pokeman.types.map( (type) => type.type.name).join(', ');
+  console.log(type);
+};
+fetchPokemon();
