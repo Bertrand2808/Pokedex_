4e2f3563b77dd46cb537d2183d74a3db9ccdbb78
Bertrand RENAUDIN <bertrand.renaudin@odigo.com>
2022-12-20 16:57:14 +0100

    WF20_12_2022

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const pokedex = document.getElementById('pokedex');
 
+const pokeCache = {};
+
 const fetchPokemon = async () => {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
   const res = await fetch(url);
   const data = await res.json();
-  const pokemon = data.results.map((result, index) => ({
-
+  const pokemons = data.results.map((result, index) => ({
     ...result,
     id: index + 1,
     image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
       index + 1
-    }.png`,
+    }.png`
   }));
-  displayPokemon(pokemon);
+  displayPokemon(pokemons);
 };
 
-const displayPokemon = (pokemon) => {
-  console.log(pokemon);
-  const pokemonHTMLString = pokemon
+const displayPokemon = (pokemons) => {
+    const pokemonHTMLString = pokemons
     .map(
-      (pokeman) =>
+      (pokemon) =>
         `
-      <li class="card" onclick="selectPokemon(${pokeman.id})">
-        <img class="card-image" src="${pokeman.image}"/>
-        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+      <li class="card" onclick="selectPokemon(${pokemon.id})">
+        <img class="card-image" src="${pokemon.image}"/>
+        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
       </li>
       `
     )
@@ -32,14 +32,52 @@ const displayPokemon = (pokemon) => {
 };
 
 const selectPokemon = async (id) => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  const res = await fetch(url);
-  const pokeman = await res.json();
-  displayPopup(pokeman);
+  if (!pokeCache[id]) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    pokeCache[id] = pokemon;
+    displayPopup(pokemon);
+  }else {
+    displayPopup(pokeCache[id]);
+  };
 };
-const displayPopup = (pokeman) => {
-  console.log(pokeman);
-  const type = pokeman.types.map( (type) => type.type.name).join(', ');
-  console.log(type);
+const displayPopup = (pokemon) => {
+  const type = pokemon.types.map((type) => type.type.name).join(', ');
+  const image = pokemon.sprites['front_default'];
+  const htmlString = `
+    <div class="popup">
+      <button class="button_close" id="closeBtn" onclick="closePopup()">Close</button>
+      <div class="card">
+          <img class="card-image" src="${image}"/>
+          <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+          <p><small>Height: </small>${pokemon.height} | <small>Weight: </small>${pokemon.weight} | <small>Type: </small>${type}
+      </div>
+    </div>
+  `;
+  pokedex.innerHTML = htmlString + pokedex.innerHTML;
 };
+
+const closePopup = () => {
+  const popup = document.querySelector('.popup');
+  popup.parentElement.removeChild(popup);
+};
+
+const searchPokemons = (search) => {
+  return pokemons.filter(pokemon => {
+    const regex = new RegExp(search, 'gi');
+    return pokemon.name.match(regex);
+  });
+};
+
+const displaySearch = () => {
+  const tabresult = searchPokemons(this.value, pokemons);
+  console.log(tabresult);
+};
+
+const inputSearch = document.querySelector('input');
+const resultat = document.querySelector('ul');
+inputSearch.addEventListener('change', displaySearch);
+inputSearch.addEventListener('keyup', displaySearch);
+
 fetchPokemon();
